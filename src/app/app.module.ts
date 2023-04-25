30343d6b388a54efb7b954aa83eaf315ed56a1b1
Francois Supervielle-Brouques <francoisup40@gmail.com>
2023-04-25 13:33:33 +0200

    serviceAC wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AircraftComponent } from './models/aircraft/aircraft.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AircraftComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
