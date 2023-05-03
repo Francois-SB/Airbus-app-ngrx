8864c1f07b14268d72a02c23fd328665782a8376
Francois Supervielle-Brouques <francoisup40@gmail.com>
2023-05-03 08:55:09 +0200

    ngrx - effects

--- src/app/states/aircraft.state.ts
+++ src/app/states/aircraft.state.ts
@@ -1,4 +1,4 @@
-/* export enum DataStateEnum {
+export enum DataStateEnum {
     LOADING,LOADED,
     ERROR
     
@@ -6,7 +6,6 @@
 
 export interface AppDataState<T> {
     dataState? : DataStateEnum,
-    data? : T,// TemplateStringsArray 
+    data? : T,/* TemplateStringsArray */
     errorMessage?:string
 }
- */
